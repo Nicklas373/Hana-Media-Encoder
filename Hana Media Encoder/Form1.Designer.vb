8c7dc2c83599728546a98ef90f55b4825c6b74b6
Dicky Herlambang (花） <herlambangdicky5@gmail.com>
2022-08-15 12:30:17 +0700

    HME: Misc changes

--- Hana Media Encoder/Form1.Designer.vb
+++ Hana Media Encoder/Form1.Designer.vb
@@ -28,7 +28,7 @@ Partial Class Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 15.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(944, 461)
+        Me.ClientSize = New System.Drawing.Size(1027, 480)
         Me.Name = "Form1"
         Me.Text = "Form1"
         Me.ResumeLayout(False)
