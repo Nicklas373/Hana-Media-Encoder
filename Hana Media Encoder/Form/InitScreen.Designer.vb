6dc2d980381ea21c09e8b3bebfae3c0db1f7ce10
Dicky Herlambang (花） <herlambangdicky5@gmail.com>
2022-08-27 12:58:02 +0700

    InitScreen: Introduce loading first screen
    * InitScreen will check all required folder and ffmpeg binary on every startup, if not passed then i will tell the result after switch into MainMenu

---
+++ Hana Media Encoder/Form/InitScreen.Designer.vb
@@ -0,0 +1,135 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class InitScreen
+    Inherits Syncfusion.WinForms.Controls.SfForm
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(InitScreen))
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.BackColor = System.Drawing.Color.Transparent
+        Me.Label1.Font = New System.Drawing.Font("Poppins", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.Label1.ForeColor = System.Drawing.Color.FromArgb(CType(CType(219, Byte), Integer), CType(CType(219, Byte), Integer), CType(CType(219, Byte), Integer))
+        Me.Label1.Location = New System.Drawing.Point(31, 307)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(49, 23)
+        Me.Label1.TabIndex = 10
+        Me.Label1.Text = "Label1"
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.BackColor = System.Drawing.Color.Transparent
+        Me.PictureBox1.BackgroundImage = CType(resources.GetObject("PictureBox1.BackgroundImage"), System.Drawing.Image)
+        Me.PictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
+        Me.PictureBox1.Location = New System.Drawing.Point(9, 26)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(288, 135)
+        Me.PictureBox1.TabIndex = 11
+        Me.PictureBox1.TabStop = False
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.BackColor = System.Drawing.Color.Transparent
+        Me.Label2.Font = New System.Drawing.Font("Poppins", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.Label2.ForeColor = System.Drawing.Color.FromArgb(CType(CType(219, Byte), Integer), CType(CType(219, Byte), Integer), CType(CType(219, Byte), Integer))
+        Me.Label2.Location = New System.Drawing.Point(31, 195)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(60, 26)
+        Me.Label2.TabIndex = 12
+        Me.Label2.Text = "Label2"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.BackColor = System.Drawing.Color.Transparent
+        Me.Label3.Font = New System.Drawing.Font("Poppins", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.Label3.ForeColor = System.Drawing.Color.FromArgb(CType(CType(219, Byte), Integer), CType(CType(219, Byte), Integer), CType(CType(219, Byte), Integer))
+        Me.Label3.Location = New System.Drawing.Point(31, 223)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(60, 26)
+        Me.Label3.TabIndex = 13
+        Me.Label3.Text = "Label3"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Font = New System.Drawing.Font("Alcubierre", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.Label4.ForeColor = System.Drawing.Color.FromArgb(CType(CType(244, Byte), Integer), CType(CType(169, Byte), Integer), CType(CType(80, Byte), Integer))
+        Me.Label4.Location = New System.Drawing.Point(31, 518)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(58, 21)
+        Me.Label4.TabIndex = 14
+        Me.Label4.Text = "Label4"
+        '
+        'InitScreen
+        '
+        Me.AllowRoundedCorners = True
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 15.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.FromArgb(CType(CType(22, Byte), Integer), CType(CType(27, Byte), Integer), CType(CType(33, Byte), Integer))
+        Me.BackgroundImage = CType(resources.GetObject("$this.BackgroundImage"), System.Drawing.Image)
+        Me.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
+        Me.ClientSize = New System.Drawing.Size(944, 594)
+        Me.ControlBox = False
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.Controls.Add(Me.Label1)
+        Me.DoubleBuffered = True
+        Me.ForeColor = System.Drawing.Color.Transparent
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.MaximizeBox = False
+        Me.MinimizeBox = False
+        Me.Name = "InitScreen"
+        Me.ShowIcon = False
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Style.BackColor = System.Drawing.Color.FromArgb(CType(CType(22, Byte), Integer), CType(CType(27, Byte), Integer), CType(CType(33, Byte), Integer))
+        Me.Style.ForeColor = System.Drawing.Color.Transparent
+        Me.Style.MdiChild.IconHorizontalAlignment = System.Windows.Forms.HorizontalAlignment.Center
+        Me.Style.MdiChild.IconVerticalAlignment = System.Windows.Forms.VisualStyles.VerticalAlignment.Center
+        Me.Style.TitleBar.BackColor = System.Drawing.Color.Transparent
+        Me.Style.TitleBar.BottomBorderColor = System.Drawing.Color.Transparent
+        Me.Style.TitleBar.ForeColor = System.Drawing.Color.WhiteSmoke
+        Me.Style.TitleBar.Height = 0
+        Me.Style.TitleBar.Padding = New System.Windows.Forms.Padding(0)
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents PictureBox1 As PictureBox
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+End Class
---
+++ Hana Media Encoder/Form/InitScreen.vb
@@ -0,0 +1,115 @@
+﻿Imports System.IO
+Imports System.Threading
+Imports Syncfusion.WinForms.Controls
+Public Class InitScreen
+    Inherits SfForm
+    Public Sub SplashLoad(sender As Object, e As EventArgs) Handles MyBase.Load
+        AllowTransparency = False
+        AllowRoundedCorners = True
+        Height = 600
+        Width = 960
+        Dim initDirRes As String
+        Dim initFFMPEGRes As String
+        Label2.Text = "Copyright (C) " & My.Application.Info.Copyright
+        Label3.Text = "Created by Dicky Herlambang"
+        Label4.Text = "v" & Strings.Left(My.Application.Info.Version.ToString, 5)
+        Me.Refresh()
+        initDirRes = Strings.Mid(InitDir(), 6)
+        Me.Refresh()
+        initFFMPEGRes = Strings.Mid(InitFFMPEG(), 6)
+        Dim EndResult As String = "START: " & initDirRes & " | " & initFFMPEGRes & " :END"
+        GC.Collect()
+        GC.WaitForPendingFinalizers()
+        File.Delete("Init_Res.txt")
+        File.Create("Init_Res.txt").Dispose()
+        Dim writer As New StreamWriter("Init_Res.txt", True)
+        writer.WriteLine(EndResult)
+        writer.Close()
+        Me.Hide()
+    End Sub
+    Private Function InitDir() As String
+        Dim retReqDir As String() = {"audioConfig", "audioStream", "videoConfig", "videoStream", "thumbnail"}
+        For Each req_dir As String In retReqDir
+            Label1.Text = "Loading: " & req_dir
+            Thread.Sleep(200)
+            Me.Refresh()
+            If Directory.Exists(req_dir) = False Then
+                Thread.Sleep(200)
+                Me.Refresh()
+                Directory.CreateDirectory(req_dir)
+            End If
+        Next
+    End Function
+    Private Function InitFFMPEG() As String
+        Dim retFFMPEGConf As String
+        Dim retFFMPEGLetter As String
+        Dim retStats As String
+        Dim retMessage As String
+        Dim ffBinaryLoad As String() = {"ffmpeg.exe", "ffplay.exe", "ffprobe.exe"}
+        Dim ffBinaryValidityLoad As String() = {"ffmpeg", "ffplay", "ffprobe"}
+        Dim ffBinaryMissLoad As New List(Of String)()
+        Dim ffBinaryValidityMissLoad As New List(Of String)()
+        Dim ffBinaryCount As Integer = 3
+        Dim ffBinaryValidityCount As Integer = 3
+        If File.Exists("config.ini") Then
+            FfmpegConfig = FindConfig("config.ini", "FFMPEG Binary:")
+            Hwdefconfig = FindConfig("config.ini", "GPU Engine:")
+            If FfmpegConfig = "null" Then
+                retStats = "False"
+                retMessage = "FFMPEG configuration was not found !" & vbCrLf & vbCrLf & "Please configure it on options menu !"
+            Else
+                retFFMPEGConf = FfmpegConfig.Remove(0, 14) & "\"
+                retFFMPEGLetter = String.Concat(retFFMPEGConf.AsSpan(0, 1), ":")
+                For Each ffBin As String In ffBinaryLoad
+                    Label1.Text = "Loading: " & ffBin
+                    Thread.Sleep(200)
+                    Me.Refresh()
+                    If File.Exists(retFFMPEGConf & ffBin) = False Then
+                        ffBinaryCount += 1
+                        ffBinaryMissLoad.Add(ffBin)
+                    End If
+                Next
+                If ffBinaryCount > 3 Then
+                    retStats = "False"
+                    retMessage = "Failed to find FFMPEG Binary !" & vbCrLf & vbCrLf & "Missing FFMPEG Binary: " & String.Join(" , ", ffBinaryMissLoad)
+                Else
+                    For Each ffres As String In ffBinaryValidityLoad
+                        Newffargs = ffres & " -hide_banner -version 2>&1"
+                        HMEGenerateAlt("HME_FFMPEG_Init.bat", retFFMPEGLetter, Chr(34) & retFFMPEGConf & Chr(34), newffargs, "")
+                        Dim psi As New ProcessStartInfo("HME_FFMPEG_Init.bat") With {
+                            .RedirectStandardError = False,
+                            .RedirectStandardOutput = True,
+                            .CreateNoWindow = True,
+                            .WindowStyle = ProcessWindowStyle.Hidden,
+                            .UseShellExecute = False
+                        }
+                        Dim process As Process = Process.Start(psi)
+                        While Not process.StandardOutput.EndOfStream
+                            Newffres = process.StandardOutput.ReadToEnd
+                            If Strings.Left(newffres, 2).Equals("ff") = False Then
+                                ffBinaryValidityCount += 1
+                                ffBinaryValidityMissLoad.Add(ffres)
+                            End If
+                        End While
+                    Next
+                    If ffBinaryValidityCount > 3 Then
+                        retStats = "False"
+                        retMessage = "Failed to initialize FFMPEG Binary !" & vbCrLf & vbCrLf & "Invalid FFMPEG Binary: " & String.Join(" , ", ffBinaryValidityMissLoad)
+                    Else
+                        If hwdefConfig = "GPU Engine:" Then
+                            retStats = "False"
+                            retMessage = "GPU HW Encoder was not configured !" & vbCrLf & vbCrLf & "Please configure it on options menu, native encoding are not supported yet !"
+                        Else
+                            retStats = True
+                            retMessage = ""
+                        End If
+                    End If
+                End If
+            End If
+        Else
+            retStats = "False"
+            retMessage = "Config file was not found !" & vbCrLf & vbCrLf & "Please configure it on options menu !"
+        End If
+        Return retStats & retMessage
+    End Function
+End Class
