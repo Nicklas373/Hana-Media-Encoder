5869297d81b8ef1312a992ef716f251eee5c8da2
Dicky Herlambang (花） <herlambangdicky5@gmail.com>
2022-08-27 13:01:14 +0700

    OTA: Re-written variable
    * Move variable into specific variable file and only load into several forms with their need without re-write or re-declare every variable if want to use

--- Hana Media Encoder/Form/OTA.vb
+++ Hana Media Encoder/Form/OTA.vb
@@ -1,42 +1,35 @@
 ﻿Imports Syncfusion.Windows.Forms
 Imports Syncfusion.WinForms.Controls
 Imports Newtonsoft.Json.Linq
+Imports Newtonsoft.Json.JsonConvert
 Imports System.Net
 Imports System.IO
 Public Class OTAMenu
     Inherits SfForm
-    Dim curParsedVer As String()
-    Dim downloadURL As String
     Dim downloadStats As Boolean = True
     Dim installStats As Boolean = False
-    Dim newParsedVer As String()
-    Dim mergedNewVer As Integer
-    Dim mergedCurVer As Integer
-    Dim OTA As String
-    Dim parsejson As JObject
-    Dim parsedchangelog As String = ""
-    Dim progPercentage As Integer
-    Dim WithEvents WC As New WebClient
+    Dim WithEvents WC_Events As New WebClient
     Public Sub OTA_Load(sender As Object, e As EventArgs) Handles MyBase.Load
         AllowTransparency = False
+        AllowRoundedCorners = True
         MessageBoxAdv.MessageBoxStyle = MessageBoxAdv.Style.Metro
         ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12
-        OTA = WC.DownloadString("https://raw.githubusercontent.com/Nicklas373/Hana-Media-Encoder/master/OTA")
-        parsejson = JObject.Parse(OTA)
-        Dim apprel = parsejson.SelectToken("release_date").ToString()
-        Dim appver = parsejson.SelectToken("version").ToString()
-        downloadURL = parsejson.SelectToken("update_url").ToString
-        Dim appchangelog = Newtonsoft.Json.JsonConvert.DeserializeObject(parsejson.SelectToken("changelog").ToString)
-        newParsedVer = appver.Split(".")
-        curParsedVer = My.Application.Info.Version.ToString.Split(".")
-        mergedNewVer = String.Join(newParsedVer(0), newParsedVer(1), newParsedVer(2))
-        mergedCurVer = String.Join(curParsedVer(0), curParsedVer(1), curParsedVer(2))
-        For Each item As String In appchangelog
-            parsedchangelog &= item & vbCrLf
+        OTA = WC_Events.DownloadString("https://raw.githubusercontent.com/Nicklas373/Hana-Media-Encoder/master/OTA")
+        Parsejson = JObject.Parse(OTA)
+        AppRel = Parsejson.SelectToken("release_date").ToString()
+        AppVer = Parsejson.SelectToken("version").ToString()
+        AppChangelog = DeserializeObject(Parsejson.SelectToken("changelog").ToString)
+        DownloadURL = Parsejson.SelectToken("update_url").ToString
+        NewParsedVer = AppVer.Split(".")
+        CurParsedVer = My.Application.Info.Version.ToString.Split(".")
+        MergedNewVer = String.Join(NewParsedVer(0), NewParsedVer(1), NewParsedVer(2))
+        MergedCurVer = String.Join(CurParsedVer(0), CurParsedVer(1), CurParsedVer(2))
+        For Each item As String In AppChangelog
+            Parsedchangelog &= item & vbCrLf
         Next
-        Label7.Text = appver.ToString
-        Label5.Text = apprel.ToString
-        RichTextBox1.Text = "What's New:" & vbCrLf & vbCrLf & parsedchangelog
+        Label7.Text = AppVer.ToString
+        Label5.Text = AppRel.ToString
+        RichTextBox1.Text = "What's New:" & vbCrLf & vbCrLf & Parsedchangelog
     End Sub
     Private Sub UpdateNow(sender As Object, e As EventArgs) Handles Button1.Click
         If installStats = True Then
@@ -57,15 +50,15 @@ Public Class OTAMenu
                     GC.WaitForPendingFinalizers()
                     File.Delete(My.Application.Info.DirectoryPath & "\HME.msi")
                 End If
-                WC.DownloadFileAsync(New Uri(downloadURL), My.Application.Info.DirectoryPath & "\HME.msi")
+                WC_Events.DownloadFileAsync(New Uri(DownloadURL), My.Application.Info.DirectoryPath & "\HME.msi")
             End If
         End If
     End Sub
-    Private Sub WC_DownloadProgressChanged(ByVal sender As Object, ByVal e As DownloadProgressChangedEventArgs) Handles WC.DownloadProgressChanged
+    Private Sub WC_DownloadProgressChanged(ByVal sender As Object, ByVal e As DownloadProgressChangedEventArgs) Handles WC_Events.DownloadProgressChanged
         downloadStats = False
         Button1.Text = "Downloading... " & e.ProgressPercentage & "%"
         If e.ProgressPercentage = 100 Then
-            progPercentage = 100
+            ProgPercentage = 100
             MessageBoxAdv.Show("Download Complete !", "Hana Media Encoder", MessageBoxButtons.OK, MessageBoxIcon.Information)
             Button1.Text = "Install Update"
             installStats = True
