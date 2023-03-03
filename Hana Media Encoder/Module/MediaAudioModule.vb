2502c0e27ca563227acd945af22eef2009efdaab
Dicky Herlambang (花） <herlambangdicky5@gmail.com>
2023-03-03 13:35:21 +0700

    MediaAudioModule: Bunch of update
    * Add more alternate codec name
    * Introduce some of function that required while media queue is active

--- Hana Media Encoder/Module/MediaAudioModule.vb
+++ Hana Media Encoder/Module/MediaAudioModule.vb
@@ -38,9 +38,9 @@
             reverse = "MP3"
         ElseIf codec = "aac" Then
             reverse = "AAC"
-        ElseIf codec = "mp2" Then
+        ElseIf codec = "mp2" Or codec = "libtwolame" Then
             reverse = "MP2"
-        ElseIf codec = "opus" Then
+        ElseIf codec = "opus" Or codec = "libopus" Then
             reverse = "OPUS"
         ElseIf codec = "flac" Then
             reverse = "FLAC"
@@ -126,5 +126,135 @@
         End If
 
         Return value
+    End Function
+    Public Function aLibraryTrs(aCodec As String, aBd As String) As String
+        Dim value As String
+        If aCodec = "MP3" Or aCodec = "MP3 Audio (*.mp3)" Then
+            value = "libmp3lame"
+        ElseIf aCodec = "AAC" Or aCodec = "Advanced Audio Coding (*.aac)" Then
+            value = "aac"
+        ElseIf aCodec = "MP2" Or aCodec = "MP2 Audio (*.mp2)" Then
+            value = "libtwolame"
+        ElseIf aCodec = "OPUS" Or aCodec = "Opus Audio (*.opus)" Then
+            value = "libopus"
+        ElseIf aCodec = "FLAC" Or aCodec = "Free Lossless Audio Codec (*.flac)" Then
+            value = "flac"
+        ElseIf aCodec = "WAV" Or aCodec = "Wave PCM (*.wav)" Then
+            If aBd = "16 Bit" Then
+                value = "pcm_s16le"
+            ElseIf aBd = "24 Bit" Then
+                value = "pcm_s24le"
+            ElseIf aBd = "32 Bit" Then
+                value = "pcm_s32le"
+            Else
+                value = "pcm_s16le"
+            End If
+        Else
+            value = ""
+        End If
+
+        Return value
+    End Function
+    Public Function AudioBitrateInfo(audioCompLvl As Integer) As String
+        Dim bitrateinf As String = ""
+        If audioCompLvl < 5 Then
+            bitrateinf = "Constant Bit Rate"
+        ElseIf audioCompLvl >= 5 Then
+            bitrateinf = "Variable Bit Rate"
+        End If
+
+        Return bitrateinf
+    End Function
+    Public Function AudioBitrateVal(audioBitRate As String, audioCodec As String) As String
+        Dim bitrateval As String = ""
+        If CInt(RemoveWhitespace(audioBitRate)) > 10 Then
+            bitrateval = audioBitRate + " kb/s"
+        ElseIf CInt(RemoveWhitespace(audioBitRate)) <= 10 Then
+            If audioCodec = "Free Lossless Audio Codec (*.flac)" Then
+                bitrateval = "Compression with level " + audioBitRate
+            Else
+                bitrateval = audioBitRate + " kb/s"
+            End If
+        End If
+
+        Return bitrateval
+    End Function
+    Public Function AudioBitrateCalc(audioFormat As String, audioCompLvl As Integer) As String
+        Dim bitrateval As String = ""
+        If audioCompLvl = 0 Then
+            If audioFormat = "MP3 Audio (*.mp3)" Then
+                bitrateval = "320"
+            ElseIf audioFormat = "Advanced Audio Coding (*.aac)" Then
+                bitrateval = "512"
+            ElseIf audioFormat = "MP2 Audio (*.mp2)" Then
+                bitrateval = "384"
+            ElseIf audioFormat = "Opus Audio (*.opus)" Then
+                bitrateval = "510"
+            ElseIf audioFormat = "Free Lossless Audio Codec (*.flac)" Then
+                bitrateval = "0"
+            End If
+        ElseIf audioCompLvl = 1 Then
+            If audioFormat = "MP3 Audio (*.mp3)" Then
+                bitrateval = "256"
+            ElseIf audioFormat = "Advanced Audio Coding (*.aac)" Then
+                bitrateval = "384"
+            ElseIf audioFormat = "MP2 Audio (*.mp2)" Then
+                bitrateval = "256"
+            ElseIf audioFormat = "Opus Audio (*.opus)" Then
+                bitrateval = "192"
+            ElseIf audioFormat = "Free Lossless Audio Codec (*.flac)" Then
+                bitrateval = "1"
+            End If
+        ElseIf audioCompLvl = 2 Then
+            If audioFormat = "MP3 Audio (*.mp3)" Then
+                bitrateval = "192"
+            ElseIf audioFormat = "Advanced Audio Coding (*.aac)" Then
+                bitrateval = "256"
+            ElseIf audioFormat = "MP2 Audio (*.mp2)" Then
+                bitrateval = "192"
+            ElseIf audioFormat = "Opus Audio (*.opus)" Then
+                bitrateval = "160"
+            ElseIf audioFormat = "Free Lossless Audio Codec (*.flac)" Then
+                bitrateval = "2"
+            End If
+        ElseIf audioCompLvl = 3 Then
+            If audioFormat = "MP3 Audio (*.mp3)" Then
+                bitrateval = "160"
+            ElseIf audioFormat = "Advanced Audio Coding (*.aac)" Then
+                bitrateval = "192"
+            ElseIf audioFormat = "MP2 Audio (*.mp2)" Then
+                bitrateval = "128"
+            ElseIf audioFormat = "Opus Audio (*.opus)" Then
+                bitrateval = "96"
+            ElseIf audioFormat = "Free Lossless Audio Codec (*.flac)" Then
+                bitrateval = "3"
+            End If
+        ElseIf audioCompLvl = 4 Then
+            If audioFormat = "MP3 Audio (*.mp3)" Then
+                bitrateval = "128"
+            ElseIf audioFormat = "Advanced Audio Coding (*.aac)" Then
+                bitrateval = "128"
+            ElseIf audioFormat = "MP2 Audio (*.mp2)" Then
+                bitrateval = "128"
+            ElseIf audioFormat = "Opus Audio (*.opus)" Then
+                bitrateval = "96"
+            ElseIf audioFormat = "Free Lossless Audio Codec (*.flac)" Then
+                bitrateval = "4"
+            End If
+        ElseIf audioCompLvl = 5 Then
+            bitrateval = "5"
+        ElseIf audioCompLvl = 6 Then
+            bitrateval = "6"
+        ElseIf audioCompLvl = 7 Then
+            bitrateval = "7"
+        ElseIf audioCompLvl = 8 Then
+            bitrateval = "8"
+        ElseIf audioCompLvl = 9 Then
+            bitrateval = "9"
+        ElseIf audioCompLvl = 10 Then
+            bitrateval = "10"
+        End If
+
+        Return bitrateval
     End Function
 End Module
