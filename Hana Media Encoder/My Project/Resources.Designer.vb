2bdf0e3a71f21cab7f0b1454481c95784f63ca5b
Dicky Herlambang (花） <herlambangdicky5@gmail.com>
2023-03-05 17:47:58 +0700

    Resources: Misc changes

--- Hana Media Encoder/My Project/Resources.Designer.vb
+++ Hana Media Encoder/My Project/Resources.Designer.vb
@@ -83,6 +83,16 @@ Namespace My.Resources
         '''<summary>
         '''  Looks up a localized resource of type System.Drawing.Bitmap.
         '''</summary>
+        Friend ReadOnly Property Open_media_W() As System.Drawing.Bitmap
+            Get
+                Dim obj As Object = ResourceManager.GetObject("Open_media-W", resourceCulture)
+                Return CType(obj,System.Drawing.Bitmap)
+            End Get
+        End Property
+        
+        '''<summary>
+        '''  Looks up a localized resource of type System.Drawing.Bitmap.
+        '''</summary>
         Friend ReadOnly Property Remove() As System.Drawing.Bitmap
             Get
                 Dim obj As Object = ResourceManager.GetObject("Remove", resourceCulture)
