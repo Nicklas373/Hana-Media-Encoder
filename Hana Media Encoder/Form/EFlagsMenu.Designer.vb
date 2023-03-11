d4c1adf1c175a61c9a5c3773df71940539a605ea
Dicky Herlambang (花） <herlambangdicky5@gmail.com>
2023-03-12 01:23:00 +0700

    HME: Introduce EFlagsMenu
    Introduce Encoding Flags Menu to show all encoding flags that was written for FFMPEG, it also remove all textbox that usually use to save encoding flags on each profile

---
+++ Hana Media Encoder/Form/EFlagsMenu.Designer.vb
@@ -0,0 +1,234 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class EFlagsMenu
+    Inherits Syncfusion.WinForms.Controls.SfForm
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As ComponentModel.ComponentResourceManager = New ComponentModel.ComponentResourceManager(GetType(EFlagsMenu))
+        TextBoxExt1 = New Syncfusion.Windows.Forms.Tools.TextBoxExt()
+        MetroSetDefaultButton1 = New MetroSet_UI.Controls.MetroSetDefaultButton()
+        MetroSetDefaultButton2 = New MetroSet_UI.Controls.MetroSetDefaultButton()
+        MetroSetComboBox1 = New MetroSet_UI.Controls.MetroSetComboBox()
+        MetroSetComboBox2 = New MetroSet_UI.Controls.MetroSetComboBox()
+        Label1 = New Label()
+        Label2 = New Label()
+        CType(TextBoxExt1, ComponentModel.ISupportInitialize).BeginInit()
+        SuspendLayout()
+        ' 
+        ' TextBoxExt1
+        ' 
+        TextBoxExt1.Anchor = AnchorStyles.Top Or AnchorStyles.Bottom Or AnchorStyles.Left Or AnchorStyles.Right
+        TextBoxExt1.BackColor = Color.FromArgb(CByte(22), CByte(27), CByte(33))
+        TextBoxExt1.BeforeTouchSize = New Size(763, 175)
+        TextBoxExt1.BorderColor = Color.FromArgb(CByte(240), CByte(169), CByte(80))
+        TextBoxExt1.BorderSides = Border3DSide.Bottom
+        TextBoxExt1.BorderStyle = BorderStyle.FixedSingle
+        TextBoxExt1.FocusBorderColor = Color.FromArgb(CByte(240), CByte(169), CByte(80))
+        TextBoxExt1.Font = New Font("Poppins", 9F, FontStyle.Regular, GraphicsUnit.Point)
+        TextBoxExt1.ForeColor = Color.FromArgb(CByte(240), CByte(169), CByte(80))
+        TextBoxExt1.Location = New Point(5, 138)
+        TextBoxExt1.Metrocolor = Color.FromArgb(CByte(240), CByte(169), CByte(80))
+        TextBoxExt1.Multiline = True
+        TextBoxExt1.Name = "TextBoxExt1"
+        TextBoxExt1.ReadOnly = True
+        TextBoxExt1.ScrollBars = ScrollBars.Horizontal
+        TextBoxExt1.ShowOverflowIndicator = True
+        TextBoxExt1.Size = New Size(763, 175)
+        TextBoxExt1.TabIndex = 2
+        TextBoxExt1.ThemeName = "Default"
+        TextBoxExt1.ThemeStyle.BackColor = Color.FromArgb(CByte(22), CByte(27), CByte(33))
+        TextBoxExt1.ThemeStyle.BorderColor = Color.FromArgb(CByte(240), CByte(169), CByte(80))
+        TextBoxExt1.ThemeStyle.DisabledBackColor = Color.FromArgb(CByte(22), CByte(27), CByte(33))
+        TextBoxExt1.ThemeStyle.DisabledBorderColor = Color.FromArgb(CByte(22), CByte(27), CByte(33))
+        TextBoxExt1.ThemeStyle.DisabledForeColor = Color.FromArgb(CByte(22), CByte(27), CByte(33))
+        TextBoxExt1.ThemeStyle.FocusedBorderColor = Color.FromArgb(CByte(240), CByte(169), CByte(80))
+        TextBoxExt1.ThemeStyle.Font = New Font("Poppins", 9F, FontStyle.Regular, GraphicsUnit.Point)
+        TextBoxExt1.ThemeStyle.ForeColor = Color.FromArgb(CByte(240), CByte(169), CByte(80))
+        TextBoxExt1.ThemeStyle.HoverBorderColor = Color.FromArgb(CByte(240), CByte(169), CByte(80))
+        ' 
+        ' MetroSetDefaultButton1
+        ' 
+        MetroSetDefaultButton1.DisabledBackColor = Color.FromArgb(CByte(22), CByte(27), CByte(33))
+        MetroSetDefaultButton1.DisabledBorderColor = Color.FromArgb(CByte(22), CByte(27), CByte(33))
+        MetroSetDefaultButton1.DisabledForeColor = Color.FromArgb(CByte(22), CByte(27), CByte(33))
+        MetroSetDefaultButton1.Font = New Font("Poppins SemiBold", 9.75F, FontStyle.Bold, GraphicsUnit.Point)
+        MetroSetDefaultButton1.HoverBorderColor = Color.FromArgb(CByte(240), CByte(169), CByte(80))
+        MetroSetDefaultButton1.HoverColor = Color.FromArgb(CByte(22), CByte(27), CByte(33))
+        MetroSetDefaultButton1.HoverTextColor = Color.FromArgb(CByte(22), CByte(27), CByte(33))
+        MetroSetDefaultButton1.IsDerivedStyle = True
+        MetroSetDefaultButton1.Location = New Point(5, 14)
+        MetroSetDefaultButton1.Name = "MetroSetDefaultButton1"
+        MetroSetDefaultButton1.NormalBorderColor = Color.FromArgb(CByte(22), CByte(27), CByte(33))
+        MetroSetDefaultButton1.NormalColor = Color.FromArgb(CByte(22), CByte(27), CByte(33))
+        MetroSetDefaultButton1.NormalTextColor = Color.FromArgb(CByte(240), CByte(169), CByte(80))
+        MetroSetDefaultButton1.PressBorderColor = Color.FromArgb(CByte(240), CByte(169), CByte(80))
+        MetroSetDefaultButton1.PressColor = Color.FromArgb(CByte(22), CByte(27), CByte(33))
+        MetroSetDefaultButton1.PressTextColor = Color.FromArgb(CByte(22), CByte(27), CByte(33))
+        MetroSetDefaultButton1.RightToLeft = RightToLeft.No
+        MetroSetDefaultButton1.Size = New Size(374, 100)
+        MetroSetDefaultButton1.Style = MetroSet_UI.Enums.Style.Custom
+        MetroSetDefaultButton1.StyleManager = Nothing
+        MetroSetDefaultButton1.TabIndex = 105
+        MetroSetDefaultButton1.ThemeAuthor = "Hana-CI Build Project"
+        MetroSetDefaultButton1.ThemeName = "Hana Media Encoder MetroSetTheme"' 
+        ' MetroSetDefaultButton2
+        ' 
+        MetroSetDefaultButton2.DisabledBackColor = Color.FromArgb(CByte(22), CByte(27), CByte(33))
+        MetroSetDefaultButton2.DisabledBorderColor = Color.FromArgb(CByte(22), CByte(27), CByte(33))
+        MetroSetDefaultButton2.DisabledForeColor = Color.FromArgb(CByte(22), CByte(27), CByte(33))
+        MetroSetDefaultButton2.Font = New Font("Poppins SemiBold", 9.75F, FontStyle.Bold, GraphicsUnit.Point)
+        MetroSetDefaultButton2.HoverBorderColor = Color.FromArgb(CByte(240), CByte(169), CByte(80))
+        MetroSetDefaultButton2.HoverColor = Color.FromArgb(CByte(22), CByte(27), CByte(33))
+        MetroSetDefaultButton2.HoverTextColor = Color.FromArgb(CByte(240), CByte(169), CByte(80))
+        MetroSetDefaultButton2.IsDerivedStyle = True
+        MetroSetDefaultButton2.Location = New Point(381, 14)
+        MetroSetDefaultButton2.Name = "MetroSetDefaultButton2"
+        MetroSetDefaultButton2.NormalBorderColor = Color.FromArgb(CByte(22), CByte(27), CByte(33))
+        MetroSetDefaultButton2.NormalColor = Color.FromArgb(CByte(22), CByte(27), CByte(33))
+        MetroSetDefaultButton2.NormalTextColor = Color.FromArgb(CByte(240), CByte(169), CByte(80))
+        MetroSetDefaultButton2.PressBorderColor = Color.FromArgb(CByte(22), CByte(27), CByte(33))
+        MetroSetDefaultButton2.PressColor = Color.FromArgb(CByte(22), CByte(27), CByte(33))
+        MetroSetDefaultButton2.PressTextColor = Color.FromArgb(CByte(240), CByte(169), CByte(80))
+        MetroSetDefaultButton2.RightToLeft = RightToLeft.No
+        MetroSetDefaultButton2.Size = New Size(370, 100)
+        MetroSetDefaultButton2.Style = MetroSet_UI.Enums.Style.Custom
+        MetroSetDefaultButton2.StyleManager = Nothing
+        MetroSetDefaultButton2.TabIndex = 109
+        MetroSetDefaultButton2.ThemeAuthor = "Hana-CI Build Project"
+        MetroSetDefaultButton2.ThemeName = "Hana Media Encoder MetroSetTheme"' 
+        ' MetroSetComboBox1
+        ' 
+        MetroSetComboBox1.AllowDrop = True
+        MetroSetComboBox1.Anchor = AnchorStyles.Top Or AnchorStyles.Left Or AnchorStyles.Right
+        MetroSetComboBox1.ArrowColor = Color.FromArgb(CByte(240), CByte(169), CByte(80))
+        MetroSetComboBox1.BackColor = Color.Transparent
+        MetroSetComboBox1.BackgroundColor = Color.FromArgb(CByte(22), CByte(27), CByte(33))
+        MetroSetComboBox1.BorderColor = Color.FromArgb(CByte(240), CByte(169), CByte(80))
+        MetroSetComboBox1.CausesValidation = False
+        MetroSetComboBox1.DisabledBackColor = Color.FromArgb(CByte(22), CByte(27), CByte(33))
+        MetroSetComboBox1.DisabledBorderColor = Color.FromArgb(CByte(22), CByte(27), CByte(33))
+        MetroSetComboBox1.DisabledForeColor = Color.FromArgb(CByte(22), CByte(27), CByte(33))
+        MetroSetComboBox1.DrawMode = DrawMode.OwnerDrawFixed
+        MetroSetComboBox1.DropDownStyle = ComboBoxStyle.DropDownList
+        MetroSetComboBox1.FlatStyle = FlatStyle.System
+        MetroSetComboBox1.Font = New Font("Poppins", 9F, FontStyle.Regular, GraphicsUnit.Point)
+        MetroSetComboBox1.FormattingEnabled = True
+        MetroSetComboBox1.IsDerivedStyle = True
+        MetroSetComboBox1.ItemHeight = 20
+        MetroSetComboBox1.Location = New Point(13, 72)
+        MetroSetComboBox1.MaxDropDownItems = 100
+        MetroSetComboBox1.Name = "MetroSetComboBox1"
+        MetroSetComboBox1.SelectedItemBackColor = Color.FromArgb(CByte(240), CByte(169), CByte(80))
+        MetroSetComboBox1.SelectedItemForeColor = Color.FromArgb(CByte(22), CByte(27), CByte(33))
+        MetroSetComboBox1.Size = New Size(349, 26)
+        MetroSetComboBox1.Style = MetroSet_UI.Enums.Style.Custom
+        MetroSetComboBox1.StyleManager = Nothing
+        MetroSetComboBox1.TabIndex = 112
+        MetroSetComboBox1.ThemeAuthor = "Hana-CI Build Project"
+        MetroSetComboBox1.ThemeName = "Hana Media Encoder MetroSetTheme"' 
+        ' MetroSetComboBox2
+        ' 
+        MetroSetComboBox2.AllowDrop = True
+        MetroSetComboBox2.ArrowColor = Color.FromArgb(CByte(240), CByte(169), CByte(80))
+        MetroSetComboBox2.BackColor = Color.Transparent
+        MetroSetComboBox2.BackgroundColor = Color.FromArgb(CByte(22), CByte(27), CByte(33))
+        MetroSetComboBox2.BorderColor = Color.FromArgb(CByte(240), CByte(169), CByte(80))
+        MetroSetComboBox2.CausesValidation = False
+        MetroSetComboBox2.DisabledBackColor = Color.FromArgb(CByte(22), CByte(27), CByte(33))
+        MetroSetComboBox2.DisabledBorderColor = Color.FromArgb(CByte(22), CByte(27), CByte(33))
+        MetroSetComboBox2.DisabledForeColor = Color.FromArgb(CByte(22), CByte(27), CByte(33))
+        MetroSetComboBox2.DrawMode = DrawMode.OwnerDrawFixed
+        MetroSetComboBox2.DropDownStyle = ComboBoxStyle.DropDownList
+        MetroSetComboBox2.FlatStyle = FlatStyle.System
+        MetroSetComboBox2.Font = New Font("Poppins", 9F, FontStyle.Regular, GraphicsUnit.Point)
+        MetroSetComboBox2.FormattingEnabled = True
+        MetroSetComboBox2.IsDerivedStyle = True
+        MetroSetComboBox2.ItemHeight = 20
+        MetroSetComboBox2.Location = New Point(394, 72)
+        MetroSetComboBox2.Name = "MetroSetComboBox2"
+        MetroSetComboBox2.SelectedItemBackColor = Color.FromArgb(CByte(240), CByte(169), CByte(80))
+        MetroSetComboBox2.SelectedItemForeColor = Color.FromArgb(CByte(22), CByte(27), CByte(33))
+        MetroSetComboBox2.Size = New Size(337, 26)
+        MetroSetComboBox2.Style = MetroSet_UI.Enums.Style.Custom
+        MetroSetComboBox2.StyleManager = Nothing
+        MetroSetComboBox2.TabIndex = 113
+        MetroSetComboBox2.ThemeAuthor = "Hana-CI Build Project"
+        MetroSetComboBox2.ThemeName = "Hana Media Encoder MetroSetTheme"' 
+        ' Label1
+        ' 
+        Label1.AutoSize = True
+        Label1.Font = New Font("Poppins", 12F, FontStyle.Regular, GraphicsUnit.Point)
+        Label1.Location = New Point(13, 29)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(114, 28)
+        Label1.TabIndex = 114
+        Label1.Text = "Media Name"' 
+        ' Label2
+        ' 
+        Label2.AutoSize = True
+        Label2.Font = New Font("Poppins", 12F, FontStyle.Regular, GraphicsUnit.Point)
+        Label2.Location = New Point(394, 29)
+        Label2.Name = "Label2"
+        Label2.Size = New Size(152, 28)
+        Label2.TabIndex = 115
+        Label2.Text = "Media Command"' 
+        ' EFlagsMenu
+        ' 
+        AutoScaleDimensions = New SizeF(7F, 15F)
+        AutoScaleMode = AutoScaleMode.Font
+        BackColor = Color.FromArgb(CByte(22), CByte(27), CByte(33))
+        ClientSize = New Size(773, 321)
+        Controls.Add(Label2)
+        Controls.Add(Label1)
+        Controls.Add(MetroSetComboBox2)
+        Controls.Add(MetroSetComboBox1)
+        Controls.Add(MetroSetDefaultButton2)
+        Controls.Add(MetroSetDefaultButton1)
+        Controls.Add(TextBoxExt1)
+        ForeColor = Color.FromArgb(CByte(240), CByte(169), CByte(80))
+        FormBorderStyle = FormBorderStyle.Fixed3D
+        Icon = CType(resources.GetObject("$this.Icon"), Icon)
+        MaximizeBox = False
+        Name = "EFlagsMenu"
+        StartPosition = FormStartPosition.CenterScreen
+        Style.BackColor = Color.FromArgb(CByte(22), CByte(27), CByte(33))
+        Style.ForeColor = Color.FromArgb(CByte(240), CByte(169), CByte(80))
+        Style.MdiChild.IconHorizontalAlignment = HorizontalAlignment.Center
+        Style.MdiChild.IconVerticalAlignment = VisualStyles.VerticalAlignment.Center
+        Style.TitleBar.BackColor = Color.FromArgb(CByte(22), CByte(27), CByte(33))
+        Style.TitleBar.BottomBorderColor = Color.FromArgb(CByte(22), CByte(27), CByte(33))
+        Style.TitleBar.CloseButtonForeColor = Color.FromArgb(CByte(240), CByte(169), CByte(80))
+        Style.TitleBar.ForeColor = Color.FromArgb(CByte(240), CByte(169), CByte(80))
+        Style.TitleBar.MinimizeButtonForeColor = Color.FromArgb(CByte(240), CByte(169), CByte(80))
+        Text = "Command Info"
+        CType(TextBoxExt1, ComponentModel.ISupportInitialize).EndInit()
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents TextBoxExt1 As Syncfusion.Windows.Forms.Tools.TextBoxExt
+    Friend WithEvents MetroSetDefaultButton1 As MetroSet_UI.Controls.MetroSetDefaultButton
+    Friend WithEvents MetroSetDefaultButton2 As MetroSet_UI.Controls.MetroSetDefaultButton
+    Friend WithEvents MetroSetComboBox1 As MetroSet_UI.Controls.MetroSetComboBox
+    Friend WithEvents MetroSetComboBox2 As MetroSet_UI.Controls.MetroSetComboBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+End Class
---
+++ Hana Media Encoder/Form/EFlagsMenu.vb
@@ -0,0 +1,127 @@
+﻿Imports System.IO
+Imports Syncfusion.WinForms.Controls
+Public Class EFlagsMenu
+    Inherits SfForm
+    Public Sub EFlagsLoad(sender As Object, e As EventArgs) Handles MyBase.Load
+        AllowTransparency = False
+        AllowRoundedCorners = True
+        MetroSetComboBox1.ForeColor = ColorTranslator.FromHtml("#F4A950")
+        MetroSetComboBox1.SelectedItemBackColor = ColorTranslator.FromHtml("#F4A950")
+        MetroSetComboBox1.SelectedItemForeColor = ColorTranslator.FromHtml("#161B21")
+        MetroSetComboBox1.DisabledBackColor = ColorTranslator.FromHtml("#161B21")
+        MetroSetComboBox1.DisabledBorderColor = ColorTranslator.FromHtml("#F4A950")
+        MetroSetComboBox1.DisabledForeColor = ColorTranslator.FromHtml("#F4A950")
+        MetroSetComboBox2.ForeColor = ColorTranslator.FromHtml("#F4A950")
+        MetroSetComboBox2.SelectedItemBackColor = ColorTranslator.FromHtml("#F4A950")
+        MetroSetComboBox2.SelectedItemForeColor = ColorTranslator.FromHtml("#161B21")
+        MetroSetComboBox2.DisabledBackColor = ColorTranslator.FromHtml("#161B21")
+        MetroSetComboBox2.DisabledBorderColor = ColorTranslator.FromHtml("#F4A950")
+        MetroSetComboBox2.DisabledForeColor = ColorTranslator.FromHtml("#F4A950")
+        MediaNameLoad()
+        If MetroSetComboBox1.Items.Count <= 0 Then
+            MetroSetComboBox1.Enabled = False
+        End If
+        If MetroSetComboBox2.Items.Count <= 0 Then
+            MetroSetComboBox2.Enabled = False
+        End If
+    End Sub
+    Public Sub MediaNameChanged(sender As Object, e As EventArgs) Handles MetroSetComboBox1.SelectedIndexChanged
+        If MetroSetComboBox1.Text.ToString IsNot "" Then
+            MediaFlagsLoad()
+        End If
+    End Sub
+    Public Sub MediaFlagsChanged(sender As Object, e As EventArgs) Handles MetroSetComboBox2.SelectedIndexChanged
+        If MetroSetComboBox1.Text.ToString IsNot "" Then
+            MediaEncodeLoad()
+        End If
+    End Sub
+    Public Sub MediaNameLoad()
+        MetroSetComboBox1.Items.Clear()
+        If MainMenu.Textbox77.Text.ToString IsNot "" Then
+            MetroSetComboBox1.Items.Add(Path.GetFileName(MainMenu.Textbox77.Text.ToString))
+        End If
+        If MainMenu.MetroSetSwitch6.Switched = True And MainMenu.DataGridView1.Rows.Count > 0 Then
+            For i As Integer = 0 To MainMenu.DataGridView1.Rows.Count - 1
+                MetroSetComboBox1.Items.Add(MainMenu.DataGridView1.Rows(i).Cells(2).Value.ToString)
+            Next
+        End If
+        If MetroSetComboBox1.Items.Count <= 0 Then
+            MetroSetComboBox1.Items.Add("No Media Command Found")
+        End If
+    End Sub
+    Public Sub MediaFlagsLoad()
+        MetroSetComboBox2.Enabled = True
+        MetroSetComboBox2.Items.Clear()
+        If File.Exists(VideoStreamFlagsPath & MetroSetComboBox1.Text.ToString & "_flags_0.txt") Then
+            MetroSetComboBox2.Items.Add("Video Command")
+        ElseIf File.Exists(VideoQueueFlagsPath & MetroSetComboBox1.Text.ToString & "_flags_0.txt") Then
+            MetroSetComboBox2.Items.Add("Video Command")
+        End If
+        If File.Exists(AudioStreamFlagsPath & MetroSetComboBox1.Text.ToString & "_flags_0.txt") Then
+            MetroSetComboBox2.Items.Add("Audio Command")
+        ElseIf File.Exists(AudioQueueFlagsPath & MetroSetComboBox1.Text.ToString & "_flags_0.txt") Then
+            MetroSetComboBox2.Items.Add("Audio Command")
+        End If
+        If File.Exists(ChapterStreamConfigPath & MetroSetComboBox1.Text.ToString & "_flags_0.txt") Then
+            MetroSetComboBox2.Items.Add("Chapter Command")
+        End If
+        If File.Exists(MuxStreamConfigPath & MetroSetComboBox1.Text.ToString & "_flags_0.txt") Then
+            MetroSetComboBox2.Items.Add("Mux Command")
+        End If
+        If File.Exists(TrimStreamConfigPath & MetroSetComboBox1.Text.ToString & "_flags_0.txt") Then
+            MetroSetComboBox2.Items.Add("Trim Command")
+        End If
+        If MetroSetComboBox2.Items.Count <= 0 Then
+            MetroSetComboBox2.Items.Add("No Command Found")
+        End If
+    End Sub
+    Public Sub MediaEncodeLoad()
+        If MetroSetComboBox2.Text.ToString = "Video Flags" Then
+            If File.Exists(VideoStreamFlagsPath & MetroSetComboBox1.Text.ToString & "_flags_0.txt") Then
+                TextBoxExt1.Text = File.ReadAllText(VideoStreamFlagsPath & MetroSetComboBox1.Text.ToString & "_flags_0.txt")
+            ElseIf File.Exists(VideoQueueFlagsPath & MetroSetComboBox1.Text.ToString & "_flags_0.txt") Then
+                TextBoxExt1.Text = File.ReadAllText(VideoQueueFlagsPath & MetroSetComboBox1.Text.ToString & "_flags_0.txt")
+            Else
+                TextBoxExt1.Text = "Command not found :("
+            End If
+        ElseIf MetroSetComboBox2.Text.ToString = "Audio Flags" Then
+            If File.Exists(AudioStreamFlagsPath & MetroSetComboBox1.Text.ToString & "_flags_0.txt") Then
+                If MainMenu.MetroSetSwitch6.Switched = False And MainMenu.DataGridView1.Rows.Count <= 0 Then
+                    FlagsCount = MainMenu.MetroSetComboBox3.Items.Count
+                    For FlagsStart = 1 To FlagsCount
+                        My.Computer.FileSystem.WriteAllText(AudioStreamFlagsPath & MetroSetComboBox1.Text.ToString & "_flags_join.txt", String.Join(" ", File.ReadAllLines(AudioStreamFlagsPath & MetroSetComboBox1.Text.ToString & "_flags_" & (FlagsStart - 1).ToString & ".txt")), True)
+                    Next
+                    If File.Exists(AudioStreamFlagsPath & MetroSetComboBox1.Text.ToString & "_flags_join.txt") Then
+                        TextBoxExt1.Text = File.ReadAllText(AudioStreamFlagsPath & MetroSetComboBox1.Text.ToString & "_flags_join.txt")
+                    End If
+                Else
+                    TextBoxExt1.Text = File.ReadAllText(AudioStreamFlagsPath & MetroSetComboBox1.Text.ToString & "_flags_0.txt")
+                End If
+            ElseIf File.Exists(AudioQueueFlagsPath & MetroSetComboBox1.Text.ToString & "_flags_0.txt") Then
+                TextBoxExt1.Text = File.ReadAllText(AudioQueueFlagsPath & MetroSetComboBox1.Text.ToString & "_flags_0.txt")
+            Else
+                TextBoxExt1.Text = "Command not found :("
+            End If
+        ElseIf MetroSetComboBox2.Text.ToString = "Chapter Flags" Then
+            If File.Exists(ChapterStreamConfigPath & MetroSetComboBox1.Text.ToString & "_flags_0.txt") Then
+                TextBoxExt1.Text = File.ReadAllText(ChapterStreamConfigPath & MetroSetComboBox1.Text.ToString & "_flags_0.txt")
+            Else
+                TextBoxExt1.Text = "Command not found :("
+            End If
+        ElseIf MetroSetComboBox2.Text.ToString = "Mux Flags" Then
+            If File.Exists(MuxStreamConfigPath & MetroSetComboBox1.Text.ToString & "_flags_0.txt") Then
+                TextBoxExt1.Text = File.ReadAllText(MuxStreamConfigPath & MetroSetComboBox1.Text.ToString & "_flags_0.txt")
+            Else
+                TextBoxExt1.Text = "Command not found :("
+            End If
+        ElseIf MetroSetComboBox2.Text.ToString = "Trim Flags" Then
+            If File.Exists(TrimStreamConfigPath & MetroSetComboBox1.Text.ToString & "_flags_0.txt") Then
+                TextBoxExt1.Text = File.ReadAllText(TrimStreamConfigPath & MetroSetComboBox1.Text.ToString & "_flags_0.txt")
+            Else
+                TextBoxExt1.Text = "Command not found :("
+            End If
+        Else
+            TextBoxExt1.Text = "Command not found :("
+        End If
+    End Sub
+End Class
