2026171b7b532b2642a5acf645eac3b454edbbaa
Dicky Herlambang (花） <herlambangdicky5@gmail.com>
2024-08-04 14:05:23 +0700

    MediaAudioAltModule: Minor Updates
    - Add new support for channel configuration for NVENCC
    - Implement logic String.IsNullOrEmpty from String in-built function on vb.net, this for better function than regular string compare to define if string or text is null or not
    - Implement multi stream support for audio when NVENCC are selected

--- Hana Media Encoder/Module/MediaAudioAltModule.vb
+++ Hana Media Encoder/Module/MediaAudioAltModule.vb
@@ -1,28 +1,28 @@
 ﻿Module MediaAudioAltModule
-    Public Function aCodecAlt(Cmbx As String, bdCmbx As String) As String
+    Public Function aCodecAlt(Cmbx As String, bdCmbx As String, aStream As String) As String
         'Combobox15.text & Combobox18.text'
         Dim value As String
         If Cmbx = "Copy" Then
-            value = " --audio-copy "
+            value = " --audio-copy " & aStream
         ElseIf Cmbx = "MP3" Or Cmbx = "MP3 Audio (*.mp3)" Then
-            value = " --audio-codec libmp3lame"
+            value = " --audio-codec " & aStream & "?" & "libmp3lame"
         ElseIf Cmbx = "AAC" Or Cmbx = "Advanced Audio Coding (*.aac)" Then
-            value = " --audio-codec aac"
+            value = " --audio-codec " & aStream & "?" & "aac"
         ElseIf Cmbx = "MP2" Or Cmbx = "MP2 Audio (*.mp2)" Then
-            value = " --audio-codec libtwolame"
+            value = " --audio-codec " & aStream & "?" & "libtwolame"
         ElseIf Cmbx = "OPUS" Or Cmbx = "Opus Audio (*.opus)" Then
-            value = " --audio-codec libopus"
+            value = " --audio-codec " & aStream & "?" & "libopus"
         ElseIf Cmbx = "FLAC" Or Cmbx = "Free Lossless Audio Codec (*.flac)" Then
-            value = " --audio-codec flac"
+            value = " --audio-codec " & aStream & "?" & "flac"
         ElseIf Cmbx = "WAV" Or Cmbx = "Wave PCM (*.wav)" Then
             If bdCmbx = "16 Bit" Then
-                value = " --audio-codec pcm_s16le"
+                value = " --audio-codec " & aStream & "?" & "pcm_s16le"
             ElseIf bdCmbx = "24 Bit" Then
-                value = " --audio-codec pcm_s24le"
+                value = " --audio-codec " & aStream & "?" & "pcm_s24le"
             ElseIf bdCmbx = "32 Bit" Then
-                value = " --audio-codec pcm_s32le"
+                value = " --audio-codec " & aStream & "?" & "pcm_s32le"
             Else
-                value = " --audio-codec pcm_s16le"
+                value = " --audio-codec " & aStream & "?" & "pcm_s16le"
             End If
         Else
             value = ""
@@ -30,24 +30,39 @@
 
         Return value
     End Function
-    Public Function aBitRateAlt(Cmbx As String) As String
+    Public Function aBitRateAlt(Cmbx As String, aStream As String) As String
         'Combobox19.text '
         Dim value As String
-        If Cmbx = "" Then
+        If String.IsNullOrEmpty(Cmbx) = True Then
             value = ""
         Else
-            value = " --audio-bitrate " & Cmbx & " "
+            value = " --audio-bitrate " & aStream & "?" & Cmbx
         End If
 
         Return value
     End Function
-    Public Function aSampleRateAlt(Cmbx As String) As String
-        'Combobox16.text '
+    Public Function aChannelAlt(Cmbx As String, aStream As String) As String
+        'Combobox33.text '
         Dim value As String
-        If Cmbx = "" Then
+        If String.IsNullOrEmpty(Cmbx) = True Then
             value = ""
         Else
-            value = " --audio-samplerate " & Cmbx & " "
+            If Cmbx = "2" Then
+                value = " --audio-stream " & aStream & "?stereo"
+            Else
+                value = " --audio-stream " & aStream & "?" & Cmbx
+            End If
+        End If
+
+        Return value
+    End Function
+    Public Function aSampleRateAlt(Cmbx As String, aStream As String) As String
+        'Combobox16.text '
+        Dim value As String
+        If String.IsNullOrEmpty(Cmbx) = True Then
+            value = ""
+        Else
+            value = " --audio-samplerate " & aStream & "?" & Cmbx
         End If
 
         Return value
